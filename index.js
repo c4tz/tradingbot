57880a63166fbd56b5feed593ddc48a75997b7a0
Jaro Z <j.zink@outlook.com>
2018-02-03 17:27:51 +0100

    api config from env

--- index.js
+++ index.js
@@ -13,18 +13,22 @@ const { includes }                      = require('lodash/fp')
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 const main = async () => {
-    if (includes(exchange)(ccxt.exchanges)) {
-        x = new ccxt[exchange]()
-    } else {
+    if (!includes(exchange)(ccxt.exchanges))
         throw 'Exchange "' + exchange + '" is not supported.'
-    }
+    if (!process.env.API_KEY || !process.env.SECRET)
+        throw 'Please set API_KEY and SECRET env variables.'
+    x = new ccxt[exchange]({
+        apiKey: process.env.API_KEY,
+        secret: process.env.SECRET
+    })
+    console.log(x.apiKey)
 }
 
-const timer = async (tickrate) => {
+const timer = async (tickrate, callback) => {
     if (tickrate < 5) throw "Tickrate is too fast"
-    main()
+    callback()
     await sleep(tickrate * 1000) // Tick every N seconds
     timer(tickrate)
 }
 
-timer(tickrate)
+main()
