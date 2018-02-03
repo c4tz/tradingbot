4061fa5accddbb4bae9fb489ab0f46aa679de7b0
Jaro Z <j.zink@outlook.com>
2018-02-03 16:43:03 +0100

    initial usage of ccxt

--- index.js
+++ index.js
@@ -6,17 +6,24 @@ function logError(err) {
 process.on('uncaughtException', logError)
 process.on('unhandledRejection', logError)
 
-const { exchange, pair, dsl, tickrate }   = require('./src/arguments.js')
-const sleep                               = require('sleep').sleep
+const { exchange, pair, dsl, tickrate } = require('./src/arguments.js')
+const ccxt                              = require ('ccxt')
+const { includes }                      = require('lodash/fp')
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 const main = async () => {
-    console.log(exchange, pair, dsl, tickrate)
+    if (includes(ccxt.exchanges, exchange)) {
+        x = new ccxt[exchange]()
+    } else {
+        throw 'Exchange "' + exchange + '" is not supported.'
+    }
 }
 
 const timer = async (tickrate) => {
     if (tickrate < 5) throw "Tickrate is too fast"
     main()
-    await sleep(tickrate) // Tick every N seconds
+    await sleep(tickrate * 1000) // Tick every N seconds
     timer(tickrate)
 }
 
