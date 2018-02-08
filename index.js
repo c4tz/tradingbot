331ca3668f76cc2494395215a24cfd32bcd1263c
Jaro <j.zink@outlook.com>
2018-02-08 18:31:02 +0100

    create exchange object while parsing arguments

--- index.js
+++ index.js
@@ -2,13 +2,27 @@
 const assert                    = require('assert')
 const param                     = require('commander')
 const { version }               = require('./package.json')
+const error                     = require ('./src/error.js')
+const ccxt                      = require ('ccxt')
 const { includes, defaultTo }   = require('lodash/fp')
+
+function createExchange(exchange) {
+    if (!includes(exchange)(ccxt.exchanges))
+        throw 'Exchange "' + exchange + '" is not supported.'
+    if (!process.env.API_KEY || !process.env.SECRET)
+        throw 'Please set API_KEY and SECRET env variables.'
+    return new ccxt[exchange]({
+        apiKey: process.env.API_KEY,
+        secret: process.env.SECRET
+    })
+}
 
 param
     .command('buy <exchange> <pair> <price>')
     .option('-t, --tickrate <n>', 'Tickrate for polling', parseInt)
     .option('-d, --dsl <n>', 'Tickrate for polling', parseInt)
     .action(function(exchange, pair, price, options){
+        exchange = createExchange(exchange)
         if (options.tickrate && options.tickrate < 5) {
             console.warn("Tickrate is too fast, setting it to 5")
             options.tickrate = 5
