5fb6fcd53fc531d841e58bbfafd1e2deecb92a12
Jaro <j.zink@outlook.com>
2018-02-18 15:08:22 +0100

    fix wrong array check

--- src/buy.js
+++ src/buy.js
@@ -1,9 +1,9 @@
-const error                         = require ('./error.js')
-const { ticker }                    = require ('./ticker.js')
-const { dsl }                       = require ('./dsl.js')
+const error                             = require ('./error.js')
+const { ticker }                        = require ('./ticker.js')
+const { dsl }                           = require ('./dsl.js')
 const { getUSDBalance, getBalance,
-        getCoin, getCurrency }      = require ('./common.js')
-const { map, isNil, split }         = require ('lodash/fp')
+        getCoin, getCurrency }          = require ('./common.js')
+const { map, isEmpty, isNil, split }   = require ('lodash/fp')
 
 
 const buy = async (exchange, pair, price, volume) => {
@@ -16,16 +16,18 @@ const buy = async (exchange, pair, price, volume) => {
     const currencyBalance = await getBalance(exchange, getCurrency(pair))
     console.log("Current currency balance:", currencyBalance)
 
-    const usdtBalance = await getUSDBalance(exchange, coin)
-    console.log("Current USD balance:", usdtBalance)
+    const usdBalance = await getUSDBalance(exchange, coin)
+    console.log("Current USD balance:", usdBalance)
 
-    if (usdtBalance < 1 && !openOrders) { // we probably did not buy the coin yet
+    if (usdBalance < 1 && isEmpty(openOrders)) { // we probably did not buy the coin yet
         const amount = (currencyBalance / 100) * volume
         const order = await exchange.createLimitBuyOrder(pair, amount, price)
+        console.log(true)
         return true
     }
 
-    if (usdtBalance > 1) { // we already bought our desired coin
+    if (usdBalance > 1) { // we already bought our desired coin
+        console.log(false)
         return false
     }
 }
